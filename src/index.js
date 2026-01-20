caee74813180da958ee512734deaddcd2cdb7791
KanuckLemonFTW <alexanderfsrp@gmail.com>
2026-01-19 20:42:57 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,44 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits, Partials } = require('discord.js');
+const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers
+    GatewayIntentBits.GuildMembers,
   ],
-  partials: [Partials.Channel]
+  partials: [Partials.Channel],
 });
 
-client.once('ready', () => {
-  console.log(`Logged in as ${client.user.tag}`);
-});
+client.commands = new Collection();
+
+// Load commands dynamically
+const commandFolders = fs.readdirSync(path.join(__dirname, 'commands'));
+for (const folder of commandFolders) {
+  const commandFiles = fs
+    .readdirSync(path.join(__dirname, 'commands', folder))
+    .filter(file => file.endsWith('.js'));
+  
+  for (const file of commandFiles) {
+    const command = require(`./commands/${folder}/${file}`);
+    if (command.data && command.execute) {
+      client.commands.set(command.data.name, command);
+    }
+  }
+}
+
+// Events
+const eventFiles = fs.readdirSync(path.join(__dirname, 'events')).filter(f => f.endsWith('.js'));
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+}
 
 client.login(process.env.DISCORD_TOKEN);
