9ec7c5ff552c42a5ad57dd146dda39f21d414c72
KanuckLemonFTW <alexanderfsrp@gmail.com>
2026-01-19 21:57:27 -0800

    Delete src/config.js

--- src/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    clientId: "YOUR_BOT_CLIENT_ID", // Discord application ID
-    guildId: "YOUR_TEST_GUILD_ID",  // Your server ID (for testing slash commands)
-    ownerID: "753300433682038956",  // Your Discord ID
-    token: process.env.BOT_TOKEN,   // Bot token from GitHub secret
-    dbFile: "./src/db.json",        // Database file
-    logChannelName: "bot-logs"      // Logging channel name
-};
