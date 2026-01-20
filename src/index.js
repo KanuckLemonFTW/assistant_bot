16c4ab27893f1a3c9ffc5ad3271f331376c8430f
KanuckLemonFTW <alexanderfsrp@gmail.com>
2026-01-19 20:37:14 -0800

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers
+  ],
+  partials: [Partials.Channel]
+});
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
