0b193af625be154e3072d9c62165d997dab86c7e
KanuckLemonFTW <alexanderfsrp@gmail.com>
2026-01-19 21:13:49 -0800

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,12 @@
 const { ownerID, logChannelName } = require("./config");
+const db = require("./database");
 
 module.exports = {
     isOwner: (id) => id === ownerID,
 
     isAdmin: (member) => {
         if (!member) return false;
-        return member.roles.cache.some(r => require("./database").getDB().adminRoles.includes(r.name));
+        return member.roles.cache.some(r => db.getDB().adminRoles.includes(r.name));
     },
 
     formatUser: (user) => `${user.tag}`,
@@ -13,5 +14,14 @@ module.exports = {
     logAction: (guild, content) => {
         const logChannel = guild.channels.cache.find(ch => ch.name === logChannelName);
         if (logChannel) logChannel.send(content);
+    },
+
+    parseTime: (str) => {
+        if (!str) return null;
+        const match = str.match(/(\d+)(s|m|h|d)/);
+        if (!match) return null;
+        const [, num, unit] = match;
+        const multipliers = { s: 1000, m: 60*1000, h: 3600*1000, d: 86400*1000 };
+        return parseInt(num) * multipliers[unit];
     }
 };
