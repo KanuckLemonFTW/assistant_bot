0f09bf76e5823ba532dfff125b4bcd128ce8b88e
KanuckLemonFTW <alexanderfsrp@gmail.com>
2026-01-19 20:53:33 -0800

    Create events.js

---
+++ src/events.js
@@ -0,0 +1,22 @@
+module.exports = (client) => {
+    client.on("ready", () => console.log(`Logged in as ${client.user.tag}`));
+
+    client.on("messageCreate", async (message) => {
+        if (message.author.bot) return;
+        const { prefix } = require("./config");
+        if (!message.content.startsWith(prefix)) return;
+
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
+        const commandName = args.shift().toLowerCase();
+        const commands = require("./commands");
+
+        if (commands[commandName]) {
+            try {
+                commands[commandName].execute(message, args);
+            } catch (err) {
+                console.error(err);
+                message.reply("There was an error executing that command.");
+            }
+        }
+    });
+};
